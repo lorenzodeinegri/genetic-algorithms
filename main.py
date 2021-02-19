78e43e0f7c55a9450d5ac15881a6cebb88fcad1b
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2021-02-19 15:29:47 +0100

    Updated genetic algorithms package version and changed the test script to check the proper functionality.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import geneticalgorithm as ga
+import geneticalgorithm2 as ga
 
 
 def f(x):
@@ -7,5 +7,5 @@ def f(x):
 
 
 var_bound = np.array([[0, 10]] * 3)
-model = ga.geneticalgorithm(function=f, dimension=3, variable_type='real', variable_boundaries=var_bound)
+model = ga.geneticalgorithm2(function=f, dimension=3, variable_type='real', variable_boundaries=var_bound)
 model.run()
